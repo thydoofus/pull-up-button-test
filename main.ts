7e7e49d538d77cfc2ec1f8928a54f04ec3bf6526
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-06 18:19:35 +0800

    ACTUAL code

--- main.ts
+++ main.ts
@@ -9,18 +9,10 @@ let switch_state = 0
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == switch_state && switch_state == 0) {
+    if (pins.digitalReadPin(DigitalPin.P1) == switch_state) {
         basic.showIcon(IconNames.Happy)
         control.waitMicros(200)
         basic.showIcon(IconNames.Heart)
         keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + "v", false)
-        switch_state = 1
-    }
-    if (pins.digitalReadPin(DigitalPin.P1) == switch_state) {
-        basic.showIcon(IconNames.Sad)
-        control.waitMicros(200)
-        basic.showIcon(IconNames.Heart)
-        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + "v", false)
-        switch_state = 0
     }
 })
