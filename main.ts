bf2842cffab3e6637f93dd5807527e3ad6052411
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-06 18:08:06 +0800

    Restore '11/4/2022, 12:23:15 PM added button A'

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
     control.waitMicros(200)
     basic.showIcon(IconNames.Heart)
-    keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.alt) + "v", false)
+    keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + "v", false)
 })
 keyboard.startKeyboardService()
 let switch_state = 0
@@ -11,12 +11,16 @@ basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == switch_state && switch_state == 0) {
         basic.showIcon(IconNames.Happy)
-        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.alt) + "v", false)
+        control.waitMicros(200)
+        basic.showIcon(IconNames.Heart)
+        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + "v", false)
         switch_state = 1
     }
     if (pins.digitalReadPin(DigitalPin.P1) == switch_state) {
         basic.showIcon(IconNames.Sad)
-        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.alt) + "v", false)
+        control.waitMicros(200)
+        basic.showIcon(IconNames.Heart)
+        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + "v", false)
         switch_state = 0
     }
 })
